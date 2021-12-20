057d2ec145e7c29e2fc84bff0377866125a3e659
Nicolas Beglinger <beglinger.nicolas@gmail.com>
2021-12-20 16:46:19 +0100

    Commented the main notebook, implemented possibility to work with nga gazetteer

--- clean_gazetteer.py
+++ clean_gazetteer.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
-def clean_gazetteer(path: str, featureclass: list):
+def clean_gazetteer(featureclass: list, **kwargs):
 
-    iter_csv = pd.read_csv(path,
-                           delimiter="\t",
-                           names=["geonameid", "name", "asciiname", "alternatenames", "latitude", "longitude",
-                                  "featureclass", "featurecode", "countrycode", "cc2", "admin1code", "admin2code",
-                                  "admin3code", "admin4code", "population", "elevation",
-                                  "dem", "timezone", "modificationdate"],
-                           iterator=True,
-                           chunksize=1000)
+    iter_csv = pd.read_csv(
+        # path,
+        # header=header
+        # delimiter="\t",
+        # names=names,
+        **kwargs,
+        iterator=True,
+        chunksize=1000
+    )
 
     # df = pd.concat([chunk[chunk['featureclass'] in featureclass] for chunk in iter_csv])
 
@@ -17,7 +18,7 @@ def clean_gazetteer(path: str, featureclass: list):
 
     for i, chunk in enumerate(iter_csv):
 
-        lyst.append(chunk[chunk["featureclass"].isin(featureclass)])
+        lyst.append(chunk[chunk["FC"].isin(featureclass)])
         # if len(lyst) == 1:
         #     break
         if i % 10 == 0:
@@ -30,4 +31,21 @@ def clean_gazetteer(path: str, featureclass: list):
 
 
 if __name__ == "__main__":
-    world = clean_gazetteer(path="data/geonames/allCountries.txt", featureclass=["A", "T", "P"])
+    # world = clean_gazetteer(
+    #     path="data/geonames/allCountries.txt",
+    #     featureclass=["A", "T", "P"],
+    #     names=["geonameid", "name", "asciiname", "alternatenames", "latitude", "longitude",
+    #             "featureclass", "featurecode", "countrycode", "cc2", "admin1code", "admin2code",
+    #             "admin3code", "admin4code", "population", "elevation",
+    #             "dem", "timezone", "modificationdate"]
+    # )
+
+
+    world = clean_gazetteer(
+        featureclass=["A", "P"],
+        filepath_or_buffer="data/gazetteer/countries.txt",
+        delimiter="\t",
+        header=0
+    )
+
+    
