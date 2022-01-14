e6b606e25bd2aa3bece15e7bcc922751cee18dae
Nicolas Beglinger <beglinger.nicolas@gmail.com>
2022-01-14 16:55:10 +0100

    Only placenames that don't appear in the most frequent n words are being compared with words;

--- html2text.py
+++ html2text.py
@@ -1,22 +1,46 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = "https://www.theguardian.com/global-development/2021/dec/21/uk-accused-of-abandoning-worlds-poor-as-aid-turned-into-colonial-investment"
-html = urlopen(url).read()
-soup = BeautifulSoup(html, features="html.parser")
+# from https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python
 
-# kill all script and style elements
-for script in soup(["script", "style"]):
-    script.extract()    # rip it out
+def html2text(url: str, onlyAlpha: bool=True) -> str:
 
-# get text
-text = soup.get_text()
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, features="html.parser")
 
-# break into lines and remove leading and trailing space on each
-lines = (line.strip() for line in text.splitlines())
-# break multi-headlines into a line each
-chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-# drop blank lines
-text = '\n'.join(chunk for chunk in chunks if chunk)
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
 
-print(text)
+    # get text
+    text = soup.get_text()
+    
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = ' '.join(chunk for chunk in chunks if chunk)
+
+    if onlyAlpha:
+        lyst = [".", ","]
+
+        # text = "".join([letter for letter in text if letter.isalpha() or letter == ' '])
+        textOnlyAlpha = ''
+        for i, letter in enumerate(text):
+            if letter not in lyst:
+                textOnlyAlpha += letter
+            
+            # if i % 10 == 0:
+            #     print(i)
+
+        return textOnlyAlpha
+    
+    return text
+
+if __name__ == "__main__":
+    print(html2text(
+        url="https://www.theguardian.com/global-development/2021/dec/21/uk-accused-of-abandoning-worlds-poor-as-aid-turned-into-colonial-investment",
+        onlyAlpha=True))
