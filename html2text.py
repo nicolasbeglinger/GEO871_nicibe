ba310e5b3200f35046b341d8c1876d31545efc91
Nicolas Beglinger <beglinger.nicolas@gmail.com>
2022-01-19 11:37:59 +0100

    Switched to nltk for places recognition Added own places (e.g. africa) that are joined to the gazetteer last commit  before linting

--- html2text.py
+++ html2text.py
@@ -3,7 +3,18 @@ from bs4 import BeautifulSoup
 
 # from https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python
 
-def html2text(url: str, onlyAlpha: bool=True) -> str:
+
+def html2text(url: str, only_alpha: bool = True) -> str:
+    """Takes a url and outputs the text
+
+    Args:
+        url (str): url of the website
+        only_alpha (bool, optional): whether only alphabetic characters should be included into the 
+        text. Defaults to True.
+
+    Returns:
+        str: string containing the text of the website
+    """
 
     html = urlopen(url).read()
     soup = BeautifulSoup(html, features="html.parser")
@@ -14,33 +25,34 @@ def html2text(url: str, onlyAlpha: bool=True) -> str:
 
     # get text
     text = soup.get_text()
-    
 
     # break into lines and remove leading and trailing space on each
     lines = (line.strip() for line in text.splitlines())
-    
+
     # break multi-headlines into a line each
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
     # drop blank lines
     text = ' '.join(chunk for chunk in chunks if chunk)
 
-    if onlyAlpha:
+    if only_alpha:
         lyst = [".", ","]
 
         # text = "".join([letter for letter in text if letter.isalpha() or letter == ' '])
-        textOnlyAlpha = ''
-        for i, letter in enumerate(text):
+        text_only_alpha = ''
+        for letter in text:
             if letter not in lyst:
-                textOnlyAlpha += letter
-            
+                text_only_alpha += letter
+
             # if i % 10 == 0:
             #     print(i)
 
-        return textOnlyAlpha
-    
+        return text_only_alpha
+
     return text
+
 
 if __name__ == "__main__":
     print(html2text(
-        url="https://www.theguardian.com/global-development/2021/dec/21/uk-accused-of-abandoning-worlds-poor-as-aid-turned-into-colonial-investment",
-        onlyAlpha=True))
+        url="https://www.theguardian.com/global-development/2021/dec/21/uk-accused-of-abandoning-\
+            worlds-poor-as-aid-turned-into-colonial-investment",
+        only_alpha=True))
